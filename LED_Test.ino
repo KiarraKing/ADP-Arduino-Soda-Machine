dc5e9f5dfbfcae9c1664f3a48bcffa5891178a10
KiarraKing <115727134+KiarraKing@users.noreply.github.com>
2022-10-17 11:18:01 -0500

    Update LED_Test.ino

--- LED_Test.ino
+++ LED_Test.ino
@@ -1,1 +1,53 @@
+// Below is for the RGB LED
+
+int greenPin = 10;
+int bluePin = A1;
+int redPin = 9;
+
+
+void setup() {
+
+  // initialize the LED as an output:
+  pinMode(LED_BUILTIN, OUTPUT);
+  // initialize serial communication:
+  Serial.begin(9600);
+
+// For LED
+
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+
+//-------------------------------
+
+}
+
+
+void loop(){ 
+  
+  Serial.println("The LED should be red.");
+  setColor(255, 0, 0);  // red
+  delay(1000);
+  
+  Serial.println("The LED should be blue.");
+  setColor(0, 255, 0);  // blue
+  delay(1000);
+  
+  Serial.println("The LED should be green.");
+  setColor(0, 0, 255);  // green
+  delay(1000);
+}
+
+
+void setColor(int red, int green, int blue)
+{
+  #ifdef COMMON_ANODE
+    red = 255 - red;
+    green = 255 - green;
+    blue = 255 - blue;
+  #endif
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue);  
+}
 
